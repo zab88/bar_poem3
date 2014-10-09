4506ef8864ad9a4c3a3c9d4450920092f44f5d81
Alexey Zabaykin <alexey.zabaykin@gmail.com>
2014-10-09 13:38:37 +0700

    json response added

--- www/ajax.php
+++ www/ajax.php
@@ -13,10 +13,15 @@ $last_id = mysqli_insert_id($link);
 
 
 //call python
-
+$python_script = dirname(__FILE__).'/../www.py '.$last_id;
+shell_exec('python '.$python_script);
 
 //read results
-
+$result = mysqli_query($link, "SELECT * FROM poems_result WHERE poem_id=".$last_id);
+while($r = mysqli_fetch_array( $result )) {
+    $res_json = json_encode($r);
+}
 
 //send results
-echo "oki_".$last_id;
+//echo "oki_".$last_id;
+echo $res_json;
--- www/js/main.js
+++ www/js/main.js
@@ -3,11 +3,23 @@ jQuery(document).ready(function(){
         $.ajax({
             type: 'POST',
             url:'ajax.php',
+            dataType: "json",
             data: {
                 poem_body: $('#poem_body').val()
             }
         }).done(function( msg ) {
             console.log(msg)
+            $('#Y1').val(msg.razmer + ' ' +msg.stop)
+            $('#Y2').val(msg.lines_num)
+            $('#Y3').val()
+            $('#Y4').val(msg.m_end)
+            $('#Y5').val(msg.g_end)
+            $('#Y6').val(msg.d_end)
+            $('#Y7').val()
+            $('#Y8').val()
+            $('#Y9').val()
+            $('#Y10').val()
+            $('#Y11').val()
         })
     })
 
