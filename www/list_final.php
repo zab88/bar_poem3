2449284649a855b0912cc85079b146398e311f75
Alexey Zabaykin <alexey.zabaykin@gmail.com>
2015-01-13 10:15:35 +0600

    view of list final changed a bit

--- www/list_final.php
+++ www/list_final.php
@@ -12,13 +12,15 @@ if (isset($_GET['year'])){
 }else{
     $where = '';
 }
-$result = mysqli_query($link, "SELECT * FROM final_01 $where ORDER BY id");
+$result = mysqli_query($link, "SELECT * FROM final_02 $where ORDER BY id");
 
 echo "<table border='2'>";
 echo '<tr>
+    <td>tid</td>
     <td>id</td>
     <td>title</td>
     <td>year</td>
+    <td>~lines</td>
     <td>lines num</td>
     <td>metric</td>
     <td>strofika</td>
@@ -27,14 +29,17 @@ echo '<tr>
 while($r = mysqli_fetch_array( $result )) {
     $poems[] = json_encode($r);
     echo "<tr>";
-
+    $lines_diff = abs($r['lines_num'] - substr_count($r['poem_body'], "\n") );
+    echo "<td valign='top'>".$r['tid']."</td>";
     echo "<td valign='top'>".$r['id']."</td>";
     echo "<td valign='top'>".$r['title']."</td>";
     echo "<td valign='top'>".$r['year']."</td>";
+    echo "<td valign='top' ".($lines_diff>3?'style="color:red"':'').">" . substr_count($r['poem_body'], "\n") . "</td>";
     echo "<td valign='top'>".$r['lines_num']."</td>";
     echo "<td valign='top'>".$r['metric']."</td>";
     echo "<td valign='top'>".$r['strofika']."</td>";
     echo "<td valign='top'><a target='_blank' href='list_detail.php?outer_id=".$r['outer_id']."'>".$r['outer_id']."</td>";
+
 
     echo "</tr>";
 }
